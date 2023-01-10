67b5f9e5c4dcb60c49e3bfcb803686cdb97ba8b2
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2023-01-10 13:58:59 +0000

    moved code into seperate cpp files

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,35 +2,17 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <thread>
+#include "ThreadCreation.cpp"
+#include "Nondeterminism.cpp"
 using namespace std;
 
-void Basic_function_no_return(string message) {
-    cout << message;
+
+void Race_conditions() {
+
 }
 
-void Creating_threads() {
-    //thread name(function-pointer, arguments)
-    //create threads 1 and 2
-    thread thread1(Basic_function_no_return, "I am a thread\n");
-    thread thread2(Basic_function_no_return, "I am another thread\n");
-    //close threads 1 and 2
-    thread1.join(); thread2.join();
-}
-
-void print_nondeterminism_string(std::string message) {
-    for (int i = 0; i < 10; i++) {
-        cout << i << " " << message;
-    }
-}
-
-void Nondeterminism() {
-    thread t1(print_nondeterminism_string, "A\n");
-    thread t2(print_nondeterminism_string, "B\n");
-    t1.join(); t2.join();
-}
 
 int main() {
-    Creating_threads();
+    Nondeterminism();
 }
