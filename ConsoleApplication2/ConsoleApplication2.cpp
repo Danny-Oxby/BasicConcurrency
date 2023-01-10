28bbeea4deb09405c3fa1c78c8631038deb652bb
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2023-01-10 14:19:27 +0000

    moved code back into same cpp file

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,18 +1,89 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+using namespace std;
 
 #include <iostream>
 #include <thread>
-#include "ThreadCreation.cpp"
-#include "Nondeterminism.cpp"
-using namespace std;
+#include <windows.h>
+#include <chrono>
 
-
-void Race_conditions() {
-
+#pragma region ThreadBasics
+void basic_function_no_return(string message) {
+    cout << message;
 }
 
+void Creating_threads() {
+    //thread name(function-pointer, arguments)
+    //create threads 1 and 2
+    thread thread1(basic_function_no_return, "I am a thread\n");
+    thread thread2(basic_function_no_return, "I am another thread\n");
+    //close threads 1 and 2
+    thread1.join(); thread2.join();
+}
+#pragma endregion
+
+#pragma region Nondeterminism
+void print_nondeterminism_string(std::string message) {
+    for (int i = 0; i < 10; i++) {
+        cout << i << " " << message;
+    }
+}
+
+void Nondeterminism() {
+    //code dosn't change but the output does (message output is random)
+    thread t1(print_nondeterminism_string, "A\n");
+    thread t2(print_nondeterminism_string, "B\n");
+    t1.join(); t2.join();
+}
+#pragma endregion
+
+#pragma region RaceCondition
+class counter
+{
+private:
+    int value;
+
+public:
+    counter() { this->value = 0; }
+    ~counter() { };
+    void add(int increment) { this->value += increment; }
+    int get() { return value; }
+};
+
+static counter racy;
+
+void incrementer()
+{ //timers make the race more obviouse
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
+    for (int i = 0; i < 100; i++)
+    {
+        racy.add(1);
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+}
+
+void decrementer()
+{
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
+    for (int i = 0; i < 100; i++)
+    {
+        racy.add(-1);
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+}
+
+int Race_condition()
+{
+    std::thread t1(incrementer);
+    std::thread t2(decrementer);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final value: " << racy.get() << std::endl;
+
+    return 0;
+}
+#pragma endregion
 
 int main() {
-    Nondeterminism();
+    Creating_threads();
 }
